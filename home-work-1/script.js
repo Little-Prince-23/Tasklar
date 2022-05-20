b594daddbf588a2222335f82b9d1b4a061b9d994
Süleyman <104515474+Little-Prince-23@users.noreply.github.com>
2022-05-20 16:52:50 +0400

    Add files via upload

---
+++ home-work-1/script.js
@@ -0,0 +1,40 @@
+/*-------------------- area_of_rectangle ---------------------------*/ 
+
+/* 
+
+let en = 10;
+let uzunluq = 20;
+let sahe = en * uzunluq;
+
+alert("Duzbucaqlinin sahesi " + sahe)
+
+*/
+
+/*-------------------- age_calculator ---------------------------*/ 
+
+/*
+
+let birthDay = 1995;
+let age = new Date().getFullYear() - birthDay;
+
+alert("Sizin yasiniz " + age)
+
+*/
+
+/*-------------------- km to mil ---------------------------*/
+
+/*
+
+let km = 0.6214;
+let user_number = 10;
+let km_to_mile = user_number * km;
+
+alert(user_number + " km = " + km_to_mile + " mile." )
+
+*/
+
+
+
+
+
+
