24033585aa14a4d04753dcaa0837e719987f4cc7
Süleyman <104515474+Little-Prince-23@users.noreply.github.com>
2022-05-29 21:11:43 +0400

    tapsiriq-3

---
+++ tapsiriq-3/tapsiriq-3.js
@@ -0,0 +1,109 @@
+//Tapsiriq 1
+
+/*
+
+let reqem = prompt(`Neçə yaşınız var? `);
+let vesiqeVar;
+if(reqem>=18){
+    vesiqeVar = prompt(`Sizin yaşınız maşın sürmək üçün uyğundur və sürücülük vəsiqəniz varsa maşın sürə bilərsiniz. Sürücülük vəsiqəniz varmı?`);
+    if(vesiqeVar === "var" || vesiqeVar === "beli"){
+        alert(`Siz qaydalara uyğun olaraq maşın sürə bilərsiniz.`)
+    }else if(vesiqeVar === "yox" || vesiqeVar === "xeyr"){
+        alert(`Təəssüfki yaşınız uyğun olsa da sürücülük vəsiqəniz olmadığı üçün siz maşın sürə bilməzsiniz.`)
+    }
+}else if(reqem<18){
+    alert(`Sizin ${reqem} yaşınız var. Maşın sürə bilmək üçün minimum 18 yaşınız tamam olmalı və sürücülük vəsiqəniz olmalıdır.`)
+}
+
+*/
+
+/* ---------------------------------------------------------------------------------------------- */
+
+//Tapsiriq 2
+
+/*
+
+let num1 = prompt(`Birinci rəqəmi daxil edin`);
+let num2 = prompt(`Ikinci reqemi daxil edin`);
+let num3 = prompt(`Ucuncu reqemi daxil edin`);
+let bigger = Math.max(num1,num2,num3);
+let smaller = Math.min(num1, num2, num3)
+alert(`En boyuk reqem ${bigger}, en kicik reqem ise ${smaller}`);
+
+*/
+
+/* ---------------------------------------------------------------------------------------------- */
+
+//Tapsiriq 3
+
+/*
+
+let number = prompt(`Bir reqem daxil edin`);
+alert(`Sizin daxil etdiyiniz ededin son reqemi ${number.slice(number.length - 1,number.length)}`);
+
+*/
+
+/* ---------------------------------------------------------------------------------------------- */
+
+//Tapsiriq 4
+/*
+
+let number1 = prompt(`0 ile 30 arasinda bir reqem daxil edin`);
+if(number1>=0 && number1<=30){
+    console.log(`${number1} teleb olunan araliqdadir`)
+}else{
+    console.log(`${number1} teleb olunan araliqda deyil!`)
+}
+
+*/
+
+/*
+
+let number2 = prompt(`30 ile 60 arasinda bir reqem daxil edin`);
+if(number2>30 && number2<=60){
+    console.log(`${number2} teleb olunan araliqdadir`)
+}else{
+    console.log(`${number2} teleb olunan araliqda deyil!`)
+}
+
+*/
+
+/*
+
+let number3 = prompt(`60 ile 80 arasinda bir reqem daxil edin`);
+if(number3>60 && number3<=80){
+    console.log(`${number3} teleb olunan araliqdadir`)
+}else{
+    console.log(`${number3} teleb olunan araliqda deyil!`)
+}
+
+*/
+
+/*
+
+let number4 = prompt(`80 ile 100 arasinda bir reqem daxil edin`);
+if(number4>80 && number4<=100){
+    console.log(`${number4} teleb olunan araliqdadir`)
+}else{
+    console.log(`${number4} teleb olunan araliqda deyil!`)
+}
+
+*/
+
+/* ---------------------------------------------------------------------------------------------- */
+
+//Tapsiriq 5
+/*
+
+let numbers1 = prompt(`Birinci reqemi daxil edin`);
+let numbers2 = prompt(`Ikinci reqemi daxil edin`);
+let numbers3 = prompt(`Ucuncu reqemi daxil edin`);
+let numbersMax = Math.max(numbers1, numbers2, numbers3);
+let numbersMin = Math.min(numbers1, numbers2, numbers3);
+
+alert(`Secdiyiniz 3 reqem icerisinden en boyuk reqem ${numbersMax},
+
+Secdiyiniz 3 reqem icerisinden en kicik reqem ${numbersMin}`);
+
+*/
+
